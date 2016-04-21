0cbc7df8f7bb7217f6da4d8cc59b7e4b56370235
Kevin Dang <overcyn@gmail.com>
2016-04-21 10:36:37 -0700

    Intial commit

--- SensorFusion/ViewController.swift
+++ SensorFusion/ViewController.swift
@@ -1,25 +1,19 @@
-//
-//  ViewController.swift
-//  SensorFusion
-//
-//  Created by Kevin Dang on 4/20/16.
-//  Copyright © 2016 Kevin Dang. All rights reserved.
-//
-
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        motionManager = CMMotionManager()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
-
+    
+    var motionManager: CMMotionManager
+    
+    override func loadView() {
+        super.loadView()
+    }
 }
 
